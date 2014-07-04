504f986d91cfcae616a98fa5c4448f7f2f5bdaeb
anandramv <anandramv@gmail.com>
2014-07-04 08:38:39 +0530

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -21,15 +21,38 @@ float _cellHeight;
 
 - (void)onEnter{
     
-    [self onEnter];
+    [super onEnter];
     [self setupGrid];
     
     self.userInteractionEnabled=YES;
 }
 
 -(void)setupGrid{
-    _cellHeight=self.contentSize.height/GIRD_COLUMNS;
-    _cellWidth=self.contentSize.width/GIRD_ROWS;
+    _cellHeight=self.contentSize.height/GIRD_ROWS;
+    _cellWidth=self.contentSize.width/GIRD_COLUMNS;
+    
+    float x=0,y=0;
+    
+    _gridArray=[NSMutableArray array];
+    
+    for(int i;i<GIRD_ROWS;i++){
+        _gridArray[i]=[NSMutableArray array];
+        x=0;
+        
+        for(int j;j<GIRD_COLUMNS;j++){
+            Creature *creature=[[Creature alloc]init];
+            creature.anchorPoint=ccp(0,0);
+            creature.position=ccp(x,y);
+            [self addChild:creature];
+            
+            
+            _gridArray[i][j]=creature;
+            creature.isAlive=YES;
+            x+=_cellWidth;
+        }
+        y+=_cellHeight;
+    }
+    
 }
 
 
